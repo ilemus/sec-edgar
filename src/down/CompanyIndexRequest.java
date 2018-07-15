d45cc4c433f498ed007a1e49b36e03eb0cef32c8
Isaac Lemus <ilemus@umich.edu>
2018-07-14 23:08:48 -0400

    add some brief documentation

--- src/down/CompanyIndexRequest.java
+++ src/down/CompanyIndexRequest.java
@@ -8,6 +8,20 @@ import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * The SEC's Edgar provides poor API to easily search for CIK based on ticker.
+ * It requires a manual process of inputing a ticker into a search.
+ * The CIK is then provided in the HTML response.
+ * 
+ * CIK or Central Index Key is used by Edgar in its file structure of company filings.
+ * 
+ * This class is responsible for loosely following the HTML pattern of the response for CIK search given a HTTPS query.
+ * 
+ * The main API is getCIK from ticker symbol.
+ * 
+ * @author yitzchak
+ *
+ */
 public class CompanyIndexRequest {
     // Use count of 10 to limit data to process
     // https://www.sec.gov/cgi-bin/browse-edgar?CIK=nvda&count=10
@@ -29,7 +43,7 @@ public class CompanyIndexRequest {
         conn = (HttpURLConnection) url.openConnection();
         
         int code = conn.getResponseCode();
-        // only 200 code is good
+        // only 200 code is good, ignore all other codes
         if (code != 200) throw new Exception("Response code has an error: " + code);
         
         // Process response from server
@@ -91,6 +105,7 @@ public class CompanyIndexRequest {
             }
         }
         
+        // N solution returns fail
         if (start == -1) return "";
         
         StringBuilder sb = new StringBuilder();
@@ -111,6 +126,7 @@ public class CompanyIndexRequest {
             }
         }
         
+        // N+N solution returns success
         return sb.toString();
     }
 }
