d942fba8c3c53293b9be98ee34ad4f6bd86ec2e1
3890042 <3890042@mobis.co.kr>
2018-07-27 15:14:48 -0400

    Moved to Maven project for test automation...

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import down.CompanyIndexRequest;
-
-public class Main {
-    public static void main(String[] args) {
-        int result = CIKRequest.getCIK("NVDA");
-        
-        System.out.println("NVDA: " + result);
-    }
-}
---
+++ src/main/java/com/sec/edgar/App.java
@@ -0,0 +1,13 @@
+package com.sec.edgar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
