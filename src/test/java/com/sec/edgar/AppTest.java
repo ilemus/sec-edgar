62fa3774a58a00f0a991069123642d9541ea3a7d
3890042 <3890042@mobis.co.kr>
2018-07-27 16:20:05 -0400

    Add test case class for CIK

--- src/test/java/com/sec/edgar/AppTest.java
+++ src/test/java/com/sec/edgar/AppTest.java
@@ -4,35 +4,35 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import com.sec.edgar.cik.CikTest;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        TestSuite suite = new TestSuite(AppTest.class);
+        suite.addTestSuite(CikTest.class);
+        return suite;
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
+    public void testApp() {
         assertTrue( true );
     }
 }
---
+++ src/test/java/com/sec/edgar/cik/CikTest.java
@@ -0,0 +1,28 @@
+package com.sec.edgar.cik;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CikTest 
+    extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CikTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCik()
+    {
+        assertTrue( true );
+    }
+}
